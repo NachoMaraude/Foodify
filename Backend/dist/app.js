76c634f414c9b045a399dab44d9c568331d19e57
NachoMaraude <maraudenacho@gmail.com>
2023-02-23 12:20:20 -0300

    render3

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
@@ -14,10 +16,19 @@ app.use(body_parser_1.default.urlencoded({ extended: true, limit: "50mb" }));
 app.use(body_parser_1.default.json({ limit: "50mb" }));
 app.use((0, morgan_1.default)("dev"));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header(
+        "Access-Control-Allow-Origin",
+        "https://foodify-ten.vercel.app/"
+    ); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    );
     next();
 });
 app.set("port", process.env.PORT || 4200);
