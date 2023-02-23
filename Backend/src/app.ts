1383fdd7ccf9caee54916f7d09cba4ffdc479031
NachoMaraude <maraudenacho@gmail.com>
2023-02-23 12:09:24 -0300

    renderr

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -13,27 +13,33 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(morgan("dev"));
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+    res.header(
+        "Access-Control-Allow-Origin",
+        "https://foodify-ten.vercel.app/"
+    ); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    next();
 });
 
-app.set("port", process.env.PORT || 4200)
+app.set("port", process.env.PORT || 4200);
 
 app.use("/api", routes);
 
 // Error catching endware.
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+    // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
 });
 
 export default app;
